8322439506753efdcc4271a4954e2a33178a3951
PAN SU NADI <pansu2970@gmail.com>
2024-01-20 22:48:50 +0630

    quick fix error

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -16,7 +16,7 @@ function Home() {
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
                 Hi There!{" "}
-                <span className="wave" role="" labelled="wave">
+                <span className="wave">
                   💜
                 </span>
               </h1>
