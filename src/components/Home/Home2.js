e26c56dc1a699b983e3daddea88633861568a164
PAN SU NADI <pansu2970@gmail.com>
2024-01-22 13:28:33 +0630

    fix error

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -100,14 +100,19 @@ function Home2() {
                 </a>
               </li>
             </ul>
-            <Row>
+            <img
+              style={{
+                width: "min(100%, 600px)",
+                display: "block",
+                margin: "0 auto",
+              }}
+              src="https://user-images.githubusercontent.com/74038190/212284115-f47cd8ff-2ffb-4b04-b5bf-4d1c14c0247f.gif"
+              alt="Gradient Line"
+            />
+            {/* <Row>
               <Col md={12} className="home-about-gif">
-                <img
-                  src="https://user-images.githubusercontent.com/74038190/212284115-f47cd8ff-2ffb-4b04-b5bf-4d1c14c0247f.gif"
-                  alt="Gradient Line"
-                />
               </Col>
-            </Row>
+            </Row> */}
           </Col>
         </Row>
       </Container>
